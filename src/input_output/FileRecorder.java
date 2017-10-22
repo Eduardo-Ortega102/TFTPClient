b5d6b8e8ed9e5dd1b2e3dc05f49fd960c770d42c
Eduardo Ortega <eduardo.ortega1002@gmail.com>
2017-10-22 01:34:05 +0100

    FileRecorder finished

--- src/input_output/FileRecorder.java
+++ src/input_output/FileRecorder.java
@@ -1,11 +1,35 @@
 package input_output;
 
-public class FileRecorder {
-    private String storagePath;
+import packets.DataPacket;
 
-    public FileRecorder(String storagePath) {
+import java.io.*;
+
+public class FileRecorder {
+    private final String filename;
+    private final String storagePath;
+    private final ByteArrayOutputStream stream;
+
+    public FileRecorder(String storagePath, String filename) {
         this.storagePath = storagePath;
+        this.filename = filename;
+        stream = new ByteArrayOutputStream();
     }
 
+    public void receive(DataPacket packet) throws IOException {
+        stream.write(packet.getDataStream());
+    }
 
+    public int amountOfBytes() {
+        return stream.size();
+    }
+
+    public void store() throws IOException {
+        FileOutputStream fileOutputStream = null;
+        try {
+            fileOutputStream = new FileOutputStream(storagePath + filename);
+            fileOutputStream.write(stream.toByteArray());
+        } finally {
+            fileOutputStream.close();
+        }
+    }
 }
--- test/input_output/FileRecorderShould.java
+++ test/input_output/FileRecorderShould.java
@@ -1,21 +1,58 @@
 package input_output;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import packets.DataPacket;
+
+import java.io.File;
+import java.time.Instant;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
 
 
 public class FileRecorderShould {
 
     private FileRecorder fileRecorder;
+    private String storagePath;
+    private String filename;
 
     @Before
     public void setUp() throws Exception {
-        fileRecorder = new FileRecorder("out/");
+        storagePath = "out/";
+        filename = "filename.txt";
+        fileRecorder = new FileRecorder(storagePath, filename);
+        File file = new File(storagePath + filename);
+        if (file.exists()) file.delete();
     }
 
     @Test
-    public void name() throws Exception {
+    public void count_the_amount_of_bytes_received() throws Exception {
+        final DataPacket packet1 = packetWithData("Hello World!\n");
+        final DataPacket packet2 = packetWithData("Bye!");
+        final int expectedLength = packet1.getDataStream().length + packet2.getDataStream().length;
+        fileRecorder.receive(packet1);
+        fileRecorder.receive(packet2);
+        assertThat(fileRecorder.amountOfBytes(), is(expectedLength));
+    }
 
+    @Test
+    public void storage_all_bytes_into_a_file() throws Exception {
+        final DataPacket packet1 = packetWithData("Hello World!\n");
+        final DataPacket packet2 = packetWithData("Bye! " + Instant.now());
+        fileRecorder.receive(packet1);
+        fileRecorder.receive(packet2);
+        fileRecorder.store();
+        assertThat(new File(storagePath + filename).exists(), is(true));
+    }
+
+    private DataPacket packetWithData(String data) {
+        DataPacket packet = mock(DataPacket.class);
+        doReturn(data.getBytes()).when(packet).getDataStream();
+        return packet;
     }
 
 }
